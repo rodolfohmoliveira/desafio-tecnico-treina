5116c282ab004b0b52220094aafd30b51b9cfcb8
rodolfohmoliveira <ruddoliv@gmail.com>
2023-10-06 07:26:56 -0300

    inclusão de usuario controller com testes de metodos básicos get,del,post,put

--- src/main/java/br/com/desafio/desafiotecnico/controller/UsuarioController.java
+++ src/main/java/br/com/desafio/desafiotecnico/controller/UsuarioController.java
@@ -9,9 +9,10 @@ import br.com.desafio.desafiotecnico.repository.UsuarioRepository;
 import jakarta.validation.Valid;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping("/usuario")
 public class UsuarioController {
 
     @PostMapping
@@ -22,6 +23,29 @@ public class UsuarioController {
     @GetMapping
     public List<Usuario> listarUsuarios() {
         return usuariorepository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarUsuarioPeloId(@PathVariable("id") Long idUsuario) {
+        usuariorepository.deleteById(idUsuario);
+    }
+
+
+    @PutMapping("/{id}")
+    public Usuario atualizarUsuarioPorId(@Valid @PathVariable("id") Long idUsuario, @RequestBody Usuario usuario) {
+        Optional<Usuario> usuarioExistente = usuariorepository.findById(idUsuario);
+        if (usuarioExistente.isPresent()) {
+            Usuario usuarioObj = usuarioExistente.get();
+
+            usuarioObj.setNome(usuario.getNome());
+            usuarioObj.setSobrenome(usuario.getSobrenome());
+            usuarioObj.setEmail(usuario.getEmail());
+            
+
+            return usuariorepository.save(usuarioObj);
+        }
+        return null;
+
     }
 
     @Autowired
